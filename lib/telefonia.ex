4d34fb015d59ef145baf551c4d47a196a058e3a3
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-08 08:57:43 -0300

    :tada: initial structure

---
+++ lib/telefonia.ex
@@ -0,0 +1,18 @@
+defmodule Telefonia do
+  @moduledoc """
+  Documentation for `Telefonia`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Telefonia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
