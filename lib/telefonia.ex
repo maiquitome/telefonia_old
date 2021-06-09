0bb508834b1ac0ffe3bdb40fd1773274c2bb714d
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-09 13:38:48 -0300

    :sparkles: add register_subscriber facade

--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -3,16 +3,12 @@ defmodule Telefonia do
   Documentation for `Telefonia`.
   """
 
+  @spec register_subscriber(String.t(), String.t(), String.t(), String.t()) ::
+          {:error, String.t()} | {:ok, String.t()}
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Telefonia.hello()
-      :world
-
+  Write the subscriber in the pos.txt file or pre.txt file.
   """
-  def hello do
-    :world
-  end
+  defdelegate register_subscriber(name, number, cpf, plan),
+    to: Subscriber,
+    as: :register
 end
